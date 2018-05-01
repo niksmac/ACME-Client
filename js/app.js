5d4f71ba8e5c3e74c0c5b89eb85657460d6dbbdc
AJ ONeal <coolaj86@gmail.com>
2018-05-01 08:29:19 +0000

    steps 1 and 2

--- js/app.js
+++ js/app.js
@@ -18,24 +18,40 @@
 
   $qs('.js-acme-form-domains').addEventListener('submit', function (ev) {
     ev.preventDefault();
-    info.identifiers = $qs('.js-acme-domains').value.split(/,/g).map(function (hostname) {
-      return { type: 'dns', value: hostname.trim() };
-    });
-
-    return BACME.directory($qs('.js-acme-directory-url').value).then(function () {
-      i += 1;
-      steps[i]();
-    });
+    steps[i].submit(ev);
+    i += 1;
   });
 
   steps[1] = function () {
     hideForms();
     $qs('.js-acme-form-domains').hidden = false;
   };
+  steps[1].submit = function () {
+    info.identifiers = $qs('.js-acme-domains').value.split(/\s*,\s*/g).map(function (hostname) {
+      return { type: 'dns', value: hostname.trim() };
+    });
+
+    return BACME.directory($qs('.js-acme-directory-url').value).then(function (directory) {
+      $qs('.js-acme-tos-url').href = directory.meta.termsOfService;
+      steps[i]();
+    });
+  };
 
   steps[2] = function () {
     hideForms();
     $qs('.js-acme-form-account').hidden = false;
+  };
+  steps[2].submit = function () {
+    info.contact = [ 'mailto:' + $qs('.js-acme-account-email').value ];
+    info.agree = $qs('.js-acme-account-tos').checked;
+    info.greenlockAgree = $qs('.js-gl-tos').checked;
+    // TODO
+    // create account key
+    // create account
+    // capture email
+    // submit challenges
+    // populate challenges in table
+    steps[i]();
   };
 
   steps[3] = function () {
