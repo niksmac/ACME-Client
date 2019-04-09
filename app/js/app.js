8ce44bc414d37501604b54616249271b1244e43d
AJ ONeal <coolaj86@gmail.com>
2019-04-09 07:50:34 +0000

    rebrand Root

--- app/js/app.js
+++ app/js/app.js
@@ -157,13 +157,13 @@
 
   function saveContact(email, domains) {
     // to be used for good, not evil
-    return window.fetch('https://api.rootprojects.org/api/rootprojects.org/public/list', {
+    return window.fetch('https://api.rootprojects.org/api/rootprojects.org/public/community', {
       method: 'POST'
     , cors: true
     , headers: new Headers({ 'Content-Type': 'application/json' })
     , body: JSON.stringify({
         address: email
-      , list: 'greenlock-domains@rootprojects.org'
+      , project: 'greenlock-domains@rootprojects.org'
       , domain: domains.join(',')
       })
     }).catch(function (err) {
